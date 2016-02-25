e82873ee48cdccdf6162cebed529d3509f46dac5
Nick Klauer <klauer@gmail.com>
2016-02-25 17:26:25 -0600

    having fun adding a handwritten parser and lexer based on a Gopher Academy post: https://blog.gopheracademy.com/advent-2014/parsers-lexers/

---
+++ token.go
@@ -0,0 +1,33 @@
+package netgearlogs
+
+type Token int
+
+const (
+	// Special tokens
+	ILLEGAL Token = iota
+	EOF
+	WS
+
+	// Literals
+	IDENT // main
+
+	// Misc chars
+	ASTERISK
+	COMMA
+	LEFT_SQUARE_BRACKET
+	RIGHT_SQUARE_BRACKET
+	COLON
+
+	// Keywords
+	DOS_ATTACK
+	WLAN_ACCESS_REJECTED
+	ACCESS_CONTROL
+	LAN_ACCESS_REMOTE
+	DHCP_IP
+	DYNAMIC_DNS
+	UPNP_SET_EVENT
+	TIME_SYNC_NTP
+	INTERNET_CONN
+	ADMIN_LOGIN
+	EMAIL_SENT
+)
